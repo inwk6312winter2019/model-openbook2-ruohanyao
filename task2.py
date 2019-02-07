a965cf3369b9a04266db93b5d88b650dbaa84f49
yaoruohan <yaoruohan@yaoruohandeMacBook-Pro.local>
2019-02-06 20:01:14 -0400

    task2.py

---
+++ task2.py
@@ -0,0 +1,17 @@
+import csv
+
+def acc_areterial(s1,s2):
+  street = []
+  restreet = []
+  r1 = open('street_centrelines.csv','r')
+  r2 = open('Bus_stops.csv','r')
+  for area in r1:
+   area = area.split(",")
+   if area[10] == 'arterial'
+   if stee not in street:
+    street.append(stee)
+  for str in street:
+  if stre not in street:
+   bus = []
+
+print(bus)
